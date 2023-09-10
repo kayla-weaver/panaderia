d639fcb5ba1a5df0687bdcfefebc777b8d12b5b4
Kayla Weaver <kaylabeth1211@gmail.com>
2023-09-09 20:25:19 -0700

    work on Bread.cs

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -3,9 +3,20 @@ namespace Bakery.Models
 
   public class Bread
   {
-  public int numBread = 10;
-  public int numFreeBread = numBread / 3 * 1;
-  public int totalCost = (numBread - numFreeBread) * 5;
-  Console.WriteLine("Total cost: $" +totalCost);
-  }
+    public int numBread;
+    public int numFreeBread;
+
+    public Bread(totalCost)
+    {
+      totalCost = (numBread- numFreeBread) *5;
+    }
+    public string CheckType()
+    {
+      return "We will see?";
+    }
+  // public int numBread = 10;
+  // public int numFreeBread = numBread / 3 * 1;
+  // public int totalCost = (numBread - numFreeBread ) * 5;
+  // Console.WriteLine("Total cost: $" +totalCost);
+  // }
 }
