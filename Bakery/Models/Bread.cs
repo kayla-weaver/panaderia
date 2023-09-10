5255a8b024174c41cb2cadcad2a0e48208ffefbc
Kayla Weaver <kaylabeth1211@gmail.com>
2023-09-09 23:00:12 -0700

    Bread.cs working again

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -5,13 +5,18 @@ namespace Bakery.Models
   {
     public int numBread;
     public int numFreeBread;
-    public int Bread1 {get; set;}
-    public int Bread2 {get; set;}
+    // public int numBread {get; set;}
+    // public int numFreeBread {get; set;}
 
     public Bread(int Quantity)
     {
       numBread = Quantity;
       numFreeBread = (numBread / 3 * 1);
+    }
+
+    public int GetNumBread()
+    {
+      return numBread;
     }
     public string CheckType()
     {
--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -8,11 +8,11 @@ namespace Bakery
   static void Main()
     {
       Bread testBread = new Bread(9);
-      Console.WriteLine($"You asked for: {testBread.numBread} loaves of bread");
+      Console.WriteLine($"You asked for: {testBread.GetNumBread()} loaves of bread");
       Console.WriteLine("Calculating how many free loaves...");
       Console.WriteLine($"Today you will recieve {testBread.numFreeBread} loaves of bread for free!");
       Console.WriteLine("Calculating cost....");
-      Console.WriteLine($"Your total is {(testBread.numBread-testBread.numFreeBread)* 5} dollars");
+      Console.WriteLine($"Your total is {(testBread.GetNumBread()-testBread.numFreeBread)* 5} dollars");
     }
   }
 }
